cdcc6f99dc5b50121ca6159bbb51475f626cdd73
Andrew Stein <andrew.stein@benchmarksolutions.com>
2011-06-25 16:04:10 -0400

    Ship graphix

--- shared/shared.js
+++ shared/shared.js
@@ -1,74 +1,76 @@
 ;
 (function() {
-        var entities = window.entities = {
-                Ship: function(x, y) {
-                        var that = this;
+  var entities = window.entities = {
+    Ship: function(x, y) {
+      var that = this;
 
-                        that._.x = x;
-                        that._.y = y;
+      that._.x = x;
+      that._.y = y;
 
-                        scene.players.push(this);
+      scene.players.push(this);
 
-                        setInterval(function() {
-                                that._.rotation += that._.rotation_delta;
+      setInterval(function() {
+        that._.rotation += that._.rotation_delta;
 
-                                that._.x += Math.cos(that._.velocity_angle) * that._.velocity;
-                                that._.y += Math.sin(that._.velocity_angle) + that._.velocity;
+        that._.x += Math.cos(that._.velocity_angle) * that._.velocity;
+        that._.y += Math.sin(that._.velocity_angle) + that._.velocity;
 
-                                if (that._.x > 900) that._.x = 0
-                                if (that._.x < 0) that._.x = 900
-                                if (that._.y > 400) that._.y = 0
-                                if (that._.y < 0) that._.y = 400
+        if (that._.x > 900) that._.x = 0
+        if (that._.x < 0) that._.x = 900
+        if (that._.y > 400) that._.y = 0
+        if (that._.y < 0) that._.y = 400
 
-                                $("#vel").html(that._.rotation)
-                        },
-                        16)
+        $("#vel").html(that._.rotation)
+        that._.rotation_delta = that._.rotation_delta * 0.99;
 
-                        this.render = function(ctx) {
+      },
+      16)
 
-                                ctx.save()
-                                ctx.translate(that._.x + 25, that._.y + 25); //that._.x, that._.y);
-                                ctx.rotate(this._.rotation);
-                                ctx.translate(-(that._.x + 25), -(that._.y + 25));
-                                ctx.fillStyle = "red";
-                                ctx.fillRect(that._.x, that._.y, 50, 50);
-                                ctx.restore();
-                        };
-                }
-        };
+      var im = new Image();
+      im.src = 'assets/ship_default.png'
 
-        entities.Ship.prototype = {
-                _: {
-                        rotation_delta: 0,
-                        thrust_delta: 0,
-                        rotation: 0,
-                        velocity_angle: 0,
-                        velocity: 0,
-                        thrust: 0,
-                        fuel: 100,
-                        x: undefined,
-                        y: undefined
-                },
-                rotate: function(degrees) {
-                        this._.rotation_delta += degrees;
-                },
-                addVelocity: function(amount) {
-                        var x = this._.velocity * Math.cos(this._.velocity_angle);
-                        var y = this._.velocity * Math.sin(this._.velocity_angle);
+      this.render = function(ctx) {
 
-                        var dx = (0.1 * amount) * Math.cos(this._.rotation);
-                        var dy = (0.1 * amount) * Math.sin(this._.rotation);
+        ctx.save()
+        ctx.translate(that._.x + 25, that._.y + 25); //that._.x, that._.y);
+        ctx.rotate(this._.rotation);
+        ctx.translate(-(that._.x + 25), -(that._.y + 25));
+        // ctx.fillStyle = "red";
+        // ctx.fillRect(that._.x, that._.y, 50, 50);
+        ctx.drawImage(im, that._.x, that._.y)
+        ctx.restore();
+      };
+    }
+  };
 
-                        this._.velocity_angle = Math.sqrt(Math.pow(x + dx, 2), Math.pow(y + dy, 2));
-                        this._.velocity = Math.atan((y + dy) / (x + dx));
+  entities.Ship.prototype = {
+    _: {
+      rotation_delta: 0,
+      thrust_delta: 0,
+      rotation: 0,
+      velocity_angle: 0,
+      velocity: 0.5,
+      thrust: 0,
+      fuel: 100,
+      x: undefined,
+      y: undefined
+    },
+    rotate: function(degrees) {
+      this._.rotation_delta += degrees;
+    },
+    addVelocity: function(amount) {
+      var x = this._.velocity * Math.cos(this._.velocity_angle);
+      var y = this._.velocity * Math.sin(this._.velocity_angle);
 
-                        this._.rotation_delta = this._.rotation_delta * 0.5;
-                        $("")
+      var dx = (0.1 * amount) * Math.cos(this._.rotation);
+      var dy = (0.1 * amount) * Math.sin(this._.rotation);
 
-                }
-        };
+      this._.velocity_angle = Math.sqrt(Math.pow(x + dx, 2), Math.pow(y + dy, 2));
+      // this._.velocity       = Math.atan((y + dy) / (x + dx));
+    }
+  };
 
-        var scene = window.scene = {
-                players: []
-        };
+  var scene = window.scene = {
+    players: []
+  };
 })();
