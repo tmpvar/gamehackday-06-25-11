1a604e2edc78c82765454c922f82a1316e59700c
Elijah Insua <tmpvar@gmail.com>
2011-06-25 12:50:58 -0400

    added basic ship

--- client/js/index.js
+++ client/js/index.js
@@ -16,73 +16,73 @@
     ROTATION_DELTA : 10 // degrees
   };
 
-
-
   window.bootstrap = function() {
     // TODO: wait for server to add an instance for this client.
     // TODO: implement ship { setThrust }
 
-    /*
-      Keybinds
-    */
-    var heldKeys = {};
-    document.addEventListener('keydown', function(ev) {
-      console.log(ev.keyCode);
-      heldKeys[ev.keyCode] = true;
-    });
 
-    document.addEventListener('keyup', function(ev) {
-      if (heldKeys[ev.keyCode]) {
-        delete heldKeys[ev.keyCode];
-      }
-    });
+    var postConnect = function() {
+      /*
+        Setup Local ship
+      */
 
-    /*
-      Track key binds
-    */
-    setInterval(function() {
-      // Up
-      if (heldKeys[38]) {
-        // Thrust forward!
-        ship.addVelocity(CONST.THRUST);
-      }
+      var ship = new entities.Ship();
 
-      // Left
-      if (heldKeys[37]) {
-        ship.rotate(-CONST.ROTATION_DELTA)
-      }
+      /*
+        Keybinds
+      */
+      var heldKeys = {};
+      document.addEventListener('keydown', function(ev) {
+        heldKeys[ev.keyCode] = true;
+      });
 
-      // Right
-      if (heldKeys[39]) {
-        ship.rotate(CONST.ROTATION_DELTA)
-      }
+      document.addEventListener('keyup', function(ev) {
+        if (heldKeys[ev.keyCode]) {
+          delete heldKeys[ev.keyCode];
+        }
+      });
 
-      // Fire!
-      if (heldKeys[32]) {
-        ship.fire();
-      }
-    }, 16);
-    
-    
-    /*
-      Render loop
-    */
-    var fps = 1000/30;
-    setTimeout(function nextFrame() {
-      
-      
-      
-      
-      setTimeout(nextFrame, fps);
-    },fps)
-    
+      /*
+        Track key binds
+      */
+      setInterval(function() {
+        // Up
+        if (heldKeys[38]) {
+          // Thrust forward!
+          ship.addVelocity(CONST.THRUST);
+        }
 
+        // Left
+        if (heldKeys[37]) {
+          ship.rotate(-CONST.ROTATION_DELTA)
+        }
+
+        // Right
+        if (heldKeys[39]) {
+          ship.rotate(CONST.ROTATION_DELTA)
+        }
+
+        // Fire!
+        if (heldKeys[32]) {
+          ship.fire();
+        }
+      }, 16);
+
+      /*
+        Render loop
+      */
+      var fps = 1000/30;
+      setTimeout(function nextFrame() {
+
+        setTimeout(nextFrame, fps);
+      },fps)
+    };
   };
 
   /*
     Spawn the current user, and send a message to the server
   */
-  window.scene
+  
 
   
 
--- shared/shared.js
+++ shared/shared.js
@@ -1,6 +1,24 @@
 ;(function() {
-  var entities = window.entities = {};
+  var entities = window.entities = {
+    ship : function() {
+    }
+  };
+  
+  entities.ship.prototype = {
+    _      : {
+      rotation : 0,
+      thrust   : 0,
+      fuel     : 100
+    },
+    rotate : function(degrees) {
+      this.rotation += degrees;
+    },
+    addThrust : function(amount) {
+      this.thrust += amount;
+    }
+  };
+  
   var scene    = window.scene    = {
     players : []
   };
-})
+});
