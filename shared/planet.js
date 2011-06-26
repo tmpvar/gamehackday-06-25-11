30263d3678cbf11d04cd9ee9885e30a5a95b0d20
Andrew Stein <andrew.stein@benchmarksolutions.com>
2011-06-25 21:51:22 -0400

    Planet

--- shared/planet.js
+++ shared/planet.js
@@ -1,3 +1,22 @@
 (function(exports) {
+  
+  exports.Planet = function() {
+
+  }
+  
+  exports.Planet.prototype = {
+    render: function(context) {
+      context.drawImage(imageCache.background.default, 0 , 0)
+
+      context.restore();
+      context.save();
+      
+      context.translate(300, 200);
+      context.scale(window.scale, window.scale);
+      context.translate(-300, -200);
+      context.drawImage(imageCache.planet.default, 200, 100)
+      context.restore()
+    }
+  }
 
 })(typeof window === 'undefined' ? exports : window);
